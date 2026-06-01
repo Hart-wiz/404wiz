572f1b61f2384d00a1a5630b31e4e0b3bc42ae73
wisdom njimogu <njimoguwisdom@gmail.com>
2026-06-02 00:22:34 +0100

    removed next

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -199,7 +199,7 @@ export default function ContactPage() {
 
                 {/* Hidden inputs for formsubmit.co */}
                 <input type="hidden" name="_captcha" value="false" />
-                <input type="hidden" name="_next" value="/contact/success" />
+                <input type="hidden" value="/contact/success" />
 
                 <button
                   type="submit"
